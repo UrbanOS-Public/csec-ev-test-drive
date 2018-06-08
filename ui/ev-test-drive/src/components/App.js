03de2099242c91b6198347499e3619e458a9b8d4
Michael Cibulskis <mcibulskis@pillartechnology.com>
2018-06-08 17:23:29 -0400

    background image plus black background

--- ui/ev-test-drive/src/components/App.js
+++ ui/ev-test-drive/src/components/App.js
@@ -6,9 +6,6 @@ import './App.css';
 const App = () => (
   <div className="App">
     <AppPages />
-    <p className="App-intro">
-      To get started, edit <code>src/App.js</code> and save to reload.
-    </p>
   </div>
 );
 
--- ui/ev-test-drive/src/components/LandingPage.js
+++ ui/ev-test-drive/src/components/LandingPage.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import topBanner from '../images/top banner.jpg';
+import logo from '../images/logo 2.svg';
+
+import './LandingPage.css';
+
 const LandingPage = ({isActive}) => (
-  <div
+  <div className="LandingPage"
     style={{
       visibility: isActive ? 'visible' : 'hidden',
-      display: isActive ? 'inherit' : 'none'
+      display: isActive ? 'inherit' : 'none',
+      backgroundImage: "url('" + topBanner + "')"
     }}
   >
+    <img src={logo} className="LandingPage-logo" alt="logo" />
     Landing Page
   </div>
 );
