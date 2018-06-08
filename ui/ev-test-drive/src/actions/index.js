8e55aee8cb5ceb9fd014b7fab28dd8cb13c3152f
Michael Cibulskis <mcibulskis@pillartechnology.com>
2018-06-08 19:41:22 -0400

    cleanup

--- ui/ev-test-drive/src/actions/index.js
+++ ui/ev-test-drive/src/actions/index.js
@@ -1,13 +1,3 @@
-export const setHeaderVisibility = isVisible => ({
-  type: 'SET_HEADER_VISIBILITY',
-  isVisible
-});
-
-export const setFooterVisibility = isVisible => ({
-  type: 'SET_FOOTER_VISIBILITY',
-  isVisible
-});
-
 export const setVisiblePage = page => ({
   type: 'SET_VISIBLE_PAGE',
   page
