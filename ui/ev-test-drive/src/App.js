5c02b6b17b7a9bb5b32d738d3c434968e8e8dfee
Michael Cibulskis <mcibulskis@pillartechnology.com>
2018-06-08 10:55:50 -0400

    added placeholder header/footer to app

--- ui/ev-test-drive/src/App.js
+++ ui/ev-test-drive/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './images/logo 2.svg';
 import './App.css';
 
 class App extends Component {
@@ -8,11 +8,14 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-header-text">Schedule a Ride</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <footer className="App-footer">
+          <p className="App-footer-text">Footer text goes here</p>
+        </footer>
       </div>
     );
   }
