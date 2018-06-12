1173bdc19bb04a8802ce3f28b370da77ee404f40
KEngan <kengan@pillartechnology.com>
2018-06-12 11:48:00 -0400

    SEC-31 Got tests passing for application, added new component

--- ui/ev-test-drive/src/app/app.module.ts
+++ ui/ev-test-drive/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 
 import { AppRoutingModule }     from './app-routing.module';
+import { NotAloneComponent } from './info/not-alone/not-alone.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NotAloneComponent
   ],
   imports: [
     BrowserModule,
