05c3d908fa43f02c4f6e77853d80738d358234e7
KEngan <kengan@pillartechnology.com>
2018-06-14 07:55:12 -0400

    SEC-30 Built and styled checkin registration page

--- ui/ev-test-drive/src/app/checkin/registration/registration.component.ts
+++ ui/ev-test-drive/src/app/checkin/registration/registration.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Helpers } from '../../app.helpers';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
 
 @Component({
   selector: 'app-registration',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor() { }
+  applicationForm: FormGroup = this.formBuilder.group({});
+  formData: any;
+  helpers = new Helpers();
+  formSubmitted: boolean = false;
+  submitApplicationFailed: boolean = false;
+  submitApplicationSuccessful: boolean = false;    
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
   }
 
+  doCancel() {
+    console.log("Cancelled!");
+  }
+
+  doSubmit() {
+    this.formSubmitted = true;
+    if (this.applicationForm.valid) {
+      this.submitApplication();
+    }
+  }
+
+  private submitApplication() {
+    const data = this.applicationForm.value;
+
+    console.log(data);
+  }
 }
