8d2ee43b2882d8b8e4953487fc0de5e875b18e9c
KEngan <kengan@pillartechnology.com>
2018-06-16 14:12:27 -0400

    SEC-34-37 Theoretically gray out times/cars on selection of other times/cars

--- ui/ev-test-drive/src/app/checkin/car-selection/car-selection.component.ts
+++ ui/ev-test-drive/src/app/checkin/car-selection/car-selection.component.ts
@@ -14,8 +14,8 @@ export class CarSelectionComponent implements OnInit {
   timeCounter = 0;
   cars: any[] = [];
   times: any[] = [];
-  selectedCar: string;
-  selectedTime: string;
+  selectedCar: any;
+  selectedTime: any;
   formattedDate: string;
 
   constructor(private evService: EVService) { }
@@ -33,9 +33,12 @@ export class CarSelectionComponent implements OnInit {
   }
 
   initializeCars(carArray) {
-    this.cars = carArray;
-    this.cars.forEach(car => {
-      car.tileId = this.carCounter++;
+    carArray.forEach(car => {
+      if (car.active) {
+        car.tileId = this.carCounter++;
+        car.times = {};
+        this.cars.push(car);
+      }
     });
     this.getTimes();
   }
@@ -52,19 +55,32 @@ export class CarSelectionComponent implements OnInit {
     this.times.forEach(time => {
       time.formattedTime = this.helpers.formatAMPM(time.startTime);
       time.tileId = this.timeCounter++;
+      if (time.availableCount == 0 || time.formattedTime.substring(0,2) == "10") {
+        time.disabled = true;
+      }
+      time.cars.forEach(timeCar => {
+        let car = this.cars.filter(c => c.id == timeCar.carId)[0];
+        car.times[time.formattedTime] = {
+          timeSlotId: timeCar.carSlotId,
+          timeTileId: time.tileId,
+          reserved: timeCar.reserved
+        };
+      });
     });
   }
 
   doSelectCar(carTileId) {
     const carList = document.getElementsByClassName("car-tile");
     const selectedTile = carList.item(carTileId);
-    if (selectedTile.classList.contains("disabled")) {
+    if (selectedTile.classList.contains("disabled")
+       || selectedTile.classList.contains("unavailable")) {
       return;
     }
 
     for (let item of Array.from(carList)) {
       if (item.id == carTileId) {
         item.classList.toggle("selected");
+        this.selectedCar = this.cars.filter(car => car.tileId === carTileId)[0];
       } else {
         item.classList.remove("selected");
       }
@@ -74,17 +90,54 @@ export class CarSelectionComponent implements OnInit {
   doSelectTime(timeTileId) {
     const timeList = document.getElementsByClassName("time");
     const selectedTile = timeList.item(timeTileId);
-    if (selectedTile.classList.contains("disabled")) {
+    if (selectedTile.classList.contains("disabled")
+       || selectedTile.classList.contains("unavailable")) {
       return;
     }
 
     for (let item of Array.from(timeList)) {
       if (item.id == timeTileId) {
         item.classList.toggle("selected");
+        this.selectedTime = this.times.filter(time => time.tileId === timeTileId)[0];
+        this.markUnavailableCars(this.selectedTime);
       } else {
         item.classList.remove("selected");
       }
     }
+  }
+
+  markUnavailableCars(time) {
+    const carList = document.getElementsByClassName("car-tile");
+
+    this.cars.forEach(car => {
+      if (car.active) {
+        let carElement = carList.item(car.tileId);
+
+        if (car.times[time.formattedTime].reserved) {
+          carElement.classList.add("unavailable");
+          carElement.classList.remove("selected");
+        } else {
+          carElement.classList.remove("unavailable");
+        }
+      } 
+    });
+  }
+
+  markUnavailableTimes(car) {
+    const timeList = document.getElementsByClassName("time");
+
+    this.times.forEach(time => {
+      if (time.availableCount > 0) {
+        let timeElement = timeList.item(time.tileId);
+
+        if (car.times[time.formattedTime].reserved) {
+          timeElement.classList.add("unavailable");
+          timeElement.classList.remove("selected");
+        } else {
+          timeElement.classList.remove("unavailable");
+        }
+      } 
+    });
   }
 
   formatDate(date: Date) {
