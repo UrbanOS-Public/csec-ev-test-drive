5289f50bedc104ef9233399701eb47a1c30e5c8a
Jarred Olson <jarred.128@gmail.com>
2018-06-19 15:20:46 -0400

    Making endpoint to get drives for the day

---
+++ terraform/LambdaApiGetScheduledDrives.tf
@@ -0,0 +1,25 @@
+module "ApiGetScheduledDrivesRole" {
+  source = "./modules/roles/create_lambda_role"
+  lambda_role_name = "ApiGetScheduledDrivesRole"
+}
+
+module "ApiGetScheduledDrivesFunction" {
+  source = "./modules/lambda/create_lambda_function_in_vpc_with_env_variables"
+  function_name = "ApiGetScheduledDrives"
+  handler = "src/api/GetScheduledDrives.handler"
+  role_arn = "${module.ApiGetScheduledDrivesRole.arn}"
+  timeout = "10"
+  description = "Api to get scheduled drives"
+  vpc_subnet_ids = [
+    "${aws_subnet.Subnet1d.id}",
+    "${aws_subnet.Subnet1b.id}"]
+  vpc_security_group_ids = [
+    "${aws_security_group.LambdaSecurityGroup.id}"]
+  environment = {
+    variables = {
+      host = "${aws_db_instance.smartexperience_mysql_db.address}"
+      user = "${var.user}"
+      password = "${var.password}"
+    }
+  }
+}
