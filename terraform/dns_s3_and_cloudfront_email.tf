89311498b3c59605f57773cdedec3eb3d1e3fe36
Jarred Olson <jarred.128@gmail.com>
2018-06-20 15:24:11 -0400

    Wiring up API Gateway to DNS and enabling US only via cloudfront

--- terraform/dns_s3_and_cloudfront_email.tf
+++ terraform/dns_s3_and_cloudfront_email.tf
@@ -21,20 +21,34 @@ resource "aws_route53_record" "ns" {
   ]
 }
 
-//resource "aws_route53_record" "cert_domain_verification" {
-//  zone_id = "${aws_route53_zone.main.zone_id}"
-//  name = "_23e0705e6d375e3b1d868773b74af662.${var.dns_name}"
-//  type = "CNAME"
-//  ttl = "30"
-//
-//  records = [
-//    "_bc6a032549e56c911c308163202f969d.acm-validations.aws."
-//  ]
-//}
+resource "aws_route53_record" "cert_domain_verification_1" {
+  zone_id = "${aws_route53_zone.main.zone_id}"
+  name = "_23e0705e6d375e3b1d868773b74af662.drivesmartcbus.com."
+  type = "CNAME"
+  ttl = "30"
 
-resource "aws_acm_certificate" "cert" {
+  records = [
+    "_bc6a032549e56c911c308163202f969d.acm-validations.aws."
+  ]
+}
+
+resource "aws_route53_record" "cert_domain_verification_2" {
+  zone_id = "${aws_route53_zone.main.zone_id}"
+  name = "_c48f53f5f883a345155a0161f024ac9d.api.drivesmartcbus.com."
+  type = "CNAME"
+  ttl = "30"
+
+  records = [
+    "_96f732a845188a80ba5b7a3c9e2543fc.acm-validations.aws."
+  ]
+}
+
+resource "aws_acm_certificate" "cert1" {
   domain_name = "${var.dns_name}"
   validation_method = "DNS"
+  subject_alternative_names = [
+    "api.${var.dns_name}"
+  ]
 }
 
 
@@ -109,11 +123,12 @@ resource "aws_cloudfront_distribution" "website" {
 
   "restrictions" {
     "geo_restriction" {
-      restriction_type = "none"
+      restriction_type = "whitelist"
+      locations = ["US"]
     }
   }
   "viewer_certificate" {
-    acm_certificate_arn = "${aws_acm_certificate.cert.id}"
+    acm_certificate_arn = "${aws_acm_certificate.cert1.id}"
     ssl_support_method = "sni-only"
   }
   custom_error_response {
@@ -267,4 +282,28 @@ resource "aws_ses_event_destination" "cloudwatch" {
     dimension_name = "dimension"
     value_source   = "emailHeader"
   }
+}
+
+resource "aws_api_gateway_domain_name" "DriveSmartCBusApiDomain" {
+  domain_name = "api.${var.dns_name}"
+  certificate_arn="${aws_acm_certificate.cert1.id}"
+}
+
+resource "aws_route53_record" "example" {
+  zone_id = "${aws_route53_zone.main.id}"
+
+  name = "${aws_api_gateway_domain_name.DriveSmartCBusApiDomain.domain_name}"
+  type = "A"
+
+  alias {
+    name                   = "${aws_api_gateway_domain_name.DriveSmartCBusApiDomain.cloudfront_domain_name}"
+    zone_id                = "${aws_api_gateway_domain_name.DriveSmartCBusApiDomain.cloudfront_zone_id}"
+    evaluate_target_health = true
+  }
+}
+
+resource "aws_api_gateway_base_path_mapping" "test" {
+  api_id      = "${aws_api_gateway_rest_api.SmartExperienceApi.id}"
+  stage_name  = "${aws_api_gateway_deployment.event_stage.stage_name}"
+  domain_name = "${aws_api_gateway_domain_name.DriveSmartCBusApiDomain.domain_name}"
 }
