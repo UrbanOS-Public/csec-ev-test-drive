8e5010251d949468f8cabfaafb48a9b57ccf2fa1
Jarred Olson <jarred.128@gmail.com>
2018-06-21 17:20:57 -0400

    Adding a jumpbox to do mysql stuff through

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -54,3 +54,53 @@
 //      "0.0.0.0/0"]
 //  }
 //}
+
+
+resource "aws_instance" "jumpbox" {
+  ami = "ami-14c5486b"
+  instance_type = "t2.micro"
+  subnet_id = "${var.subnet-us-east-1-a}"
+  tags {
+    Name = "Jumpbox"
+  }
+  key_name = "PillarSmartExperienceKey"
+  vpc_security_group_ids = ["${aws_security_group.jumpbox_security_group.id}"]
+  associate_public_ip_address = true
+}
+
+resource "aws_security_group" "jumpbox_security_group" {
+  name = "Jumpbox Security Group"
+  tags {
+    Name = "Jumpbox Security Group"
+  }
+  description = "Rules for Jumpbox"
+  vpc_id = "${var.vpc_id}"
+
+  ingress {
+    from_port = "22"
+    to_port = "22"
+    protocol = "TCP"
+    cidr_blocks = [
+      "207.238.255.132/32",
+      "12.246.159.190/32"
+    ]
+    description = "Pillar CMH Forge IP Address"
+  }
+  ingress {
+    from_port = "22"
+    to_port = "22"
+    protocol = "TCP"
+    cidr_blocks = [
+      "67.149.70.17/32"
+    ]
+    description = "Jarred Home IP Address"
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = [
+      "0.0.0.0/0"]
+  }
+}
