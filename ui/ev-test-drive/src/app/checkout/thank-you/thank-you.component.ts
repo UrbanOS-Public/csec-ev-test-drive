898e99fb622fe7cfdb24507e36ff103d8a62088c
KEngan <kengan@pillartechnology.com>
2018-06-21 13:33:49 -0400

    SEC-44 Add thank you screen

--- ui/ev-test-drive/src/app/checkout/thank-you/thank-you.component.ts
+++ ui/ev-test-drive/src/app/checkout/thank-you/thank-you.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-thank-you',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ThankYouComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+    localStorage.clear();
   }
 
+  onDone() {
+    this.router.navigateByUrl('/checkout');
+  }
 }
