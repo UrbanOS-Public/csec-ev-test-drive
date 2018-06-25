ad901306a08b8f94b8581933f87381953c46e1ec
KEngan <kengan@pillartechnology.com>
2018-06-25 14:22:09 -0400

    Updated deploy scripts: deleted deploy.sh, added build to deployProd

--- ui/ev-test-drive/deploy.sh
+++
@@ -1,4 +0,0 @@
-#/bin/bash
-
-#upload files
-aws s3 cp ./dist/ev-test-drive s3://smart-experience-webapp --profile smart_experience --recursive --acl public-read
--- ui/ev-test-drive/deployProd.sh
+++ ui/ev-test-drive/deployProd.sh
@@ -1,4 +1,7 @@
 #/bin/bash
 
+#build code
+ng build --prod --aot
+
 #upload files
 aws s3 cp ./dist/ev-test-drive s3://smart-experience-web --profile smart_experience --recursive --acl public-read
