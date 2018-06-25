bc2ffb6fa9f2ac0c6dc3dc8f3331c205f35cd5ad
Jarred Olson <jarred.128@gmail.com>
2018-06-25 14:31:58 -0400

    tying survey to a drive

--- lambda/src/api/SaveSurvey.js
+++ lambda/src/api/SaveSurvey.js
@@ -12,13 +12,12 @@ class SaveSurvey {
     }
 
     handleEvent(event, context, callback) {
-        console.log(`HERE`);
-        console.log(event);
         const body = JSON.parse(event.body);
-        console.log(JSON.stringify(body, null, 4));
-        this.getUser(body.email)
-            .then((userRows) => this.validateUser(userRows, body.email))
-            .then((user) => this.saveUserResponse(user, body))
+        const email = body.email;
+        const confirmation_number = body.confirmationNumber;
+        this.getUser(email)
+            .then((userRows) => this.validateUser(userRows, email))
+            .then((user) => this.saveUserResponse(user, confirmation_number, body))
             .then((insertForUserResponse) => this.saveUserResponseAnswers(insertForUserResponse, body))
             .then((data) => this.successHandler(callback, data), (error) => this.errorHandler(callback, error))
         ;
@@ -29,16 +28,17 @@ class SaveSurvey {
     }
 
     validateUser(userRows, email) {
-        if(userRows.length !== 1) {
+        if (userRows.length !== 1) {
             return Promise.reject(USER_NOT_FOUND + email);
         }
         return userRows[0];
     }
 
-    saveUserResponse(user, body) {
+    saveUserResponse(user, confirmation_number, body) {
         const object = {
             user_id: user.id,
-            survey_id: body.surveyId
+            survey_id: body.surveyId,
+            confirmation_number: confirmation_number
         };
         return this.pool.doQuery("insert into user_response set ?", object);
     }
@@ -47,6 +47,7 @@ class SaveSurvey {
         const values = body.responses.map((response) => {
             return [insertForUserResponse.insertId, response.questionId, response.optionId, response.text];
         });
+        console.log(values);
         return this.pool.doQuery("insert into user_response_answer (`user_response_id`, `survey_question_id`, `survey_question_option_id`, `text`) values ?", [values]);
     }
 
@@ -60,7 +61,7 @@ class SaveSurvey {
         this.pool.end();
         console.log(`ERROR: ${error}`);
         var errorToSend = 'An error occurred when processing your request.';
-        if(error !== undefined && error.indexOf(USER_NOT_FOUND) === 0) {
+        if (error !== undefined && error.indexOf(USER_NOT_FOUND) === 0) {
             errorToSend = error;
         }
         this.ApiHelpers.httpResponse(callback, 500, {errors: errorToSend});
