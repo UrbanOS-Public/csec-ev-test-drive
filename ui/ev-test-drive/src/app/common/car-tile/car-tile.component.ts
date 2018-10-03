a94dd264fb1991f3162e08cd7b6f101dbdd17a59
Timothy Regan <tregan@pillartechnology.com>
2018-10-03 09:36:43 -0400

    redoing car tile changes

--- ui/ev-test-drive/src/app/common/car-tile/car-tile.component.ts
+++ ui/ev-test-drive/src/app/common/car-tile/car-tile.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { HostListener } from "@angular/core";
 
 @Component({
   selector: 'car-tile',
@@ -7,13 +8,28 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class CarTileComponent implements OnInit {
   @Input() car: any = { specs: {} };
+  @Input() collapsed = false;
   totalRangeText: string = "";
+  screenWidth: number = 0;
 
-  constructor() { }
+  constructor() { 
+    this.onResize();
+  }
 
   ngOnInit() {
     if (this.car.type.indexOf("BEV") == -1) {
       this.totalRangeText = "(" + this.car.specs.totalRange + " total with gas)";
+    }
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event?) {
+    this.screenWidth = window.innerWidth;
+  }
+
+  onCollapse() {
+    if(this.screenWidth <= 599) {
+      this.collapsed = !this.collapsed;
     }
   }
 
