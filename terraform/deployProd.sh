66556f06b51f887d823a0de59f851fbb3c799ea2
Timothy Regan <tregan@pillartechnology.com>
2018-10-09 10:26:31 -0400

    release tweaks

--- terraform/deployProd.sh
+++ terraform/deployProd.sh
@@ -3,4 +3,4 @@
 terraform init
 terraform get
 # You will need to put your access key and secret key in to your local ~/.aws/smart-experience.tfvars file
-terraform apply -var-file="~/.aws/smart-experience.tfvars" -var-file="variables/prod.tfvars" -state=state/prod/terraform.tfstate
+terraform apply -parallelism=0 -var-file="variables/prod.tfvars" -state=state/prod/terraform.tfstate
