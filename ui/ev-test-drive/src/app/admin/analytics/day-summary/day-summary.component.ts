9bb3759003755435836caebdccbef9c0bd14575b
Timothy Regan <tregan@pillartechnology.com>
2018-10-10 13:59:04 -0400

    create day componenent and refactor css for tables to mixin

---
+++ ui/ev-test-drive/src/app/admin/analytics/day-summary/day-summary.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit, OnChanges, Input } from '@angular/core';
+import _ from 'underscore';
+import * as moment from 'moment';
+import { Angular5Csv } from 'angular5-csv/Angular5-csv';
+
+@Component({
+  selector: 'app-day-summary',
+  templateUrl: './day-summary.component.html',
+  styleUrls: ['./day-summary.component.scss']
+})
+export class DaySummaryComponent implements OnInit, OnChanges {
+  @Input() sourceData;
+  rows = [{day:null,drivesPercent:null, drives:null}];
+  columns = ["day", "drives", "drivesPercent"];
+  dayOrder = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  ngOnChanges() {
+    if (this.sourceData.length > 0) {
+      this.rows = [];
+      var byDay = _.groupBy(this.sourceData, (drive) => {
+        return moment(drive.date).format('dddd');
+      });
+      this.dayOrder.forEach((day) => {
+        this.rows.push({day:day, drivesPercent: Math.round((byDay[day].length / this.sourceData.length * 100)), drives:byDay[day].length});
+      });
+      _.each(byDay, (drives, day) => {
+        
+      });
+    }
+  }
+
+  doExport() {
+    var headers = ["Day", "Drives", "% of Drives"];
+    new Angular5Csv(this.rows, 'DaySummaryMetrics', {headers:headers});
+  }
+
+}
