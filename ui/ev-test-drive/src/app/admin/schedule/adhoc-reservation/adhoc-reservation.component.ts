70e5c0f9a55c2782d33d71d5c7f8e1a4a5e9e8af
Drew Burns <aburns@pillartechnology.com>
2018-10-15 10:39:14 -0400

    Added placeholder material expansion component for adhoc vehicle reservation

---
+++ ui/ev-test-drive/src/app/admin/schedule/adhoc-reservation/adhoc-reservation.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-adhoc-reservation',
+  templateUrl: './adhoc-reservation.component.html',
+  styleUrls: ['./adhoc-reservation.component.scss']
+})
+export class AdhocReservationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
