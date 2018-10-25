40a6934d35200769d7244700c48e99f47f1bf1f6
Timothy Regan <tregan@pillartechnology.com>
2018-10-25 08:24:24 -0400

    schedule changes

--- sql/schedule_update.sql
+++ sql/schedule_update.sql
@@ -1,9 +1,13 @@
-update schedule set start_time = '11:00:00' where day_of_the_week = 0;
-update schedule set start_time = '11:00:00' where day_of_the_week = 6;
-update schedule set end_time = '18:00:00' where day_of_the_week = 0;
-update schedule set end_time = '18:00:00' where day_of_the_week = 6;
+update schedule set employees_per_slot = 0 where day_of_the_week = 1;
 
-update schedule set end_time = '18:00:00' where day_of_the_week = 1;
-update schedule set end_time = '18:00:00' where day_of_the_week = 2;
-update schedule set end_time = '18:00:00' where day_of_the_week = 3;
+update schedule set start_time = '11:00:00' where day_of_the_week = 2;
+update schedule set end_time = '13:00:00' where day_of_the_week = 2;
 
+update schedule set start_time = '11:00:00' where day_of_the_week = 3;
+update schedule set end_time = '13:00:00' where day_of_the_week = 3;
+
+update schedule set start_time = '11:00:00' where day_of_the_week = 4;
+update schedule set end_time = '20:00:00' where day_of_the_week = 4;
+
+update schedule set start_time = '11:00:00' where day_of_the_week = 5;
+update schedule set end_time = '20:00:00' where day_of_the_week = 5;
