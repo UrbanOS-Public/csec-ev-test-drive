2806c5b670e4cd1b741f4fdd73aea747683e68f7
Timothy Regan <tregan@pillartechnology.com>
2018-12-05 08:42:52 -0500

    Google Analytics code

--- ui/ev-test-drive/src/app/app.component.ts
+++ ui/ev-test-drive/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(private router: Router) {
+    this.router.events.subscribe(event => {
+     if (event instanceof NavigationEnd) {
+       (<any>window).ga('set', 'page', event.urlAfterRedirects);
+       (<any>window).ga('send', 'pageview');
+     }
+   });
+ }
   title = 'EV Test Drive Application';
 }
