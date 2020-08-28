1b9cc6bae6a5a94c38aa72f138fcb73d3bd62ec3
Timothy Regan <tregan@pillartechnology.com>
2020-08-28 08:12:07 -0400

    comment out this

--- terraform/dns_s3_and_cloudfront_email.tf
+++ terraform/dns_s3_and_cloudfront_email.tf
@@ -17,16 +17,15 @@ resource "aws_route53_record" "ns" {
   ]
 }
 
-resource "aws_route53_record" "cert_domain_verification_1" {
-  zone_id = "${aws_route53_zone.main.zone_id}"
-  name = "_23e0705e6d375e3b1d868773b74af662.drivesmartcbus.com."
-  type = "CNAME"
-  ttl = "30"
-
-  records = [
-    "_bc6a032549e56c911c308163202f969d.acm-validations.aws."
-  ]
-}
+//resource "aws_route53_record" "cert_domain_verification_1" {
+//  zone_id = "${aws_route53_zone.main.zone_id}"
+//  name = "_23e0705e6d375e3b1d868773b74af662.drivesmartcbus.com."
+//  type = "CNAME"
+//  ttl = "30"
+//   records = [
+//     "_bc6a032549e56c911c308163202f969d.acm-validations.aws."
+//   ]
+// }
 //
 //resource "aws_route53_record" "cert_domain_verification_2" {
 //  zone_id = "${aws_route53_zone.main.zone_id}"
